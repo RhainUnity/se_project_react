a564b4d8fe4f17408b623f7154bf03bb57bf4848
JeremyRhain <schmidtjeremyr@gmail.com>
2025-09-02 17:23:35 -0700

    commit 8

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -1,5 +1,5 @@
 import "./ItemModal.css";
-import { closeBtn } from "../../utils/constants";
+import { closeBtn } from "../../utils/constants.js";
 
 function ItemModal({ activeModal, closeModal, card }) {
   return (
