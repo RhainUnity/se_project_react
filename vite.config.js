309b2219532ae151c262ec18acd0bc48c413b565
RhainUnity <schmidtjeremyr@gmail.com>
2025-12-13 14:54:28 -0800

    commit 12-13-25-254

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const isProd = process.env.NODE_ENV === "production";
+//const isProd = process.env.NODE_ENV === "production";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: isProd ? "/se_project_react/" : "/",
+  // base: isProd ? "/se_project_react/" : "/",
+  base: "/",
   plugins: [react()],
   server: {
     port: 3000,
